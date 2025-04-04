22eb3a6a824c2bc695c6936fd51bf2f2a7ed5c72
Darren Song <dazzasong@gmail.com>
2025-04-04 18:24:37 +1100

    Removed some stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { Box, List, ListItem, ListItemText, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import PortFolioCard from "./PortfolioCard";
-import Note from "./Note";
 import Website from "./Website";
 
 function App() {
-  const languages = ["Python", "C#", "Javascript"];
   const websites = [
     {img: "Chess", link: ""},
     {img: "snake2.0.png", link: "https://snaketwo.netlify.app/"},
@@ -15,20 +13,11 @@ function App() {
     <Stack direction="row" justifyContent="space-between">
       <Box>
         <Typography variant="h4" fontWeight="bold">Hi.</Typography>
-        <Typography>I use:</Typography>
-        <List dense>
-          {languages.map((l) => (
-            <ListItem>
-              <ListItemText>{l}</ListItemText>
-            </ListItem>
-          ))}
-        </List>
-        <Typography>These are the websites/games I have made so far and some I'm still working on:</Typography>
+        <Typography>I use Python, Javascript, and C#.</Typography>
+        <Typography>Here are the websites/games I have created:</Typography>
         <Stack direction="row">
           {websites.map((web) => <Website img={web.img} link={web.link} />)}
         </Stack>
-        <Typography>I have created a notepad below, feel free to use it! (Not saved for next time)</Typography>
-        <Note />
       </Box>
       <PortFolioCard />
     </Stack>
--- src/Note.jsx
+++
@@ -1,9 +0,0 @@
-import { Box, TextField } from "@mui/material";
-
-export default function Note() {
-  return (
-    <Box width={240} bgcolor="yellow">
-      <TextField fullWidth multiline rows={8} />
-    </Box>
-  );
-}
