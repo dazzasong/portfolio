c61667572f6aead03f6d99d30ba8f94275221767
Darren Song <dazzasong@gmail.com>
2025-04-09 17:17:10 +1000

    Button updates

--- src/TheButton.jsx
+++ src/TheButton.jsx
@@ -1,24 +1,48 @@
-import { Box, Stack } from "@mui/material";
+import { useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 export default function TheButton() {
+  const [src, setSrc] = useState("the-button-assets/imgs/button.png");
+  const [text, setText] = useState();
+  const [bananasNum, setBananasNum] = useState(0);
+
   const press = () => {
-    const randomInt = Math.floor(Math.random() * 24);
+    setText();
+    const randomInt = Math.floor(Math.random() * 3);
     switch (randomInt) {
       case 0:
-        window.close()
+        setText("Hey! That hurt!");
         break;
       case 1:
-        alert("We have detected a virus on your device. Please install Microsoft Virus Defender.")
+        if (src === "the-button-assets/imgs/button.png") setSrc("the-button-assets/imgs/button2.png");
+        else setSrc("the-button-assets/imgs/button.png")
+        break;
+      case 2:
+        alert("We have detected a virus on your device. Please install Microsoft Virus Defender.");
+        break;
       default:
         return;
     }
   }
 
   return (
-    <Stack justifyContent="center" alignItems="center" height="100vh">
-      <Box sx={{ cursor: "grab" }}>
-        <img src="the-button-assets/imgs/button.png" alt="red button" width={150} onClick={press} />
-      </Box>
-    </Stack>
+    <Box>
+      <Typography position="absolute">Banana counter: {bananasNum}</Typography>
+      <Stack
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Typography fontSize={24}>{text}</Typography>
+        <Box sx={{ cursor: "pointer" }}>
+          <img
+            src={src}
+            alt="red button"
+            width={150}
+            onClick={press}
+          />
+        </Box>
+      </Stack>
+    </Box>
   )
 }
