90780c70d72d0ec302402ff06627c4a4d10baab2
Darren Song <dazzasong@gmail.com>
2025-04-11 10:23:20 +1000

    disabled in-development games

--- src/Home/MainContent/WebsiteList.jsx
+++ src/Home/MainContent/WebsiteList.jsx
@@ -1,9 +1,9 @@
 import { Card, CardActionArea, CardMedia, Stack, Typography } from "@mui/material";
 
-function WebsiteCard({ img, to }) {
+function WebsiteCard({ img, to, disabled=false }) {
   return (
-    <Card>
-      <CardActionArea href={to}>
+    <Card sx={{ opacity: disabled ? 0.6 : 1 }}>
+      <CardActionArea href={to} disabled={disabled}>
         <CardMedia component="img" image={img} height={200} />
       </CardActionArea>
     </Card>
@@ -14,16 +14,17 @@ export default function WebsiteList() {
   const websites = [
     {img: "icons/snake2.0.png", to: "snake2"},
     {img: "icons/chess.png", to: "https://singleplayerchess.netlify.app/"},
-    {img: "icons/pizza-help.png", to: "pizzahelp"},
-    {img: "icons/the-button.png", to: "thebutton"}
+    {img: "icons/pizza-help.png", to: "pizzahelp", disabled: true},
+    {img: "icons/the-button.png", to: "thebutton", disabled: true}
   ];
 
   return (
     <Stack alignItems="center" spacing={4}>
     <Typography variant="h5" fontWeight="bold"><u>Games</u> ({websites.length})</Typography>
     <Stack direction="row" spacing={2}>
-      {websites.map((web) => <WebsiteCard img={web.img} to={web.to} />)}
+      {websites.map((web) => <WebsiteCard img={web.img} to={web.to} disabled={web.disabled} />)}
     </Stack>
+    <Typography>Disabled games are still in development</Typography>
   </Stack>
   );
 }
