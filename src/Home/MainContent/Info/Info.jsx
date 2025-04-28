ae5ac46173f7059afbca43f54f9711994863339f
Darren Song <dazzasong@gmail.com>
2025-04-28 18:02:32 +1000

    credit

--- src/Home/MainContent/Info/Info.jsx
+++ src/Home/MainContent/Info/Info.jsx
@@ -5,19 +5,20 @@ export default function Info() {
   return (
     <Stack direction="row" justifyContent="space-between">
       <Box>
-        <Typography variant="h6" fontStyle="italic">Who am I?</Typography>
+        <Typography variant="h6" fontStyle='italic'>Who am I?</Typography>
         <Typography>My name is Darren (aka Dazza). I am a year 9 student.</Typography>
-        <Typography variant="h6" fontStyle="italic">What do I want to do?</Typography>
+        <Typography variant="h6" fontStyle='italic'>What do I want to do?</Typography>
         <Typography>I'm working towards making a website full of (actually fun) unblocked games that you can play at school.</Typography>
-        <Typography variant="h6" fontStyle="italic">Why?</Typography>
+        <Typography variant="h6" fontStyle='italic'>Why?</Typography>
         <Typography>I was bored.</Typography>
-        <Typography variant="h6" fontStyle="italic">What do I use?</Typography>
+        <Typography variant="h6" fontStyle='italic'>What do I use?</Typography>
         <Typography>The languages I use are Python, Javascript, and C#. The games below were made using Javascript.</Typography>
         <Box mt={2}>
           <audio controls>
             <source src="info-read.mp3" type="audio/mpeg" />
           </audio>
         </Box>
+        <Typography>Read-aloud provided by Jackson Bruyn.</Typography>
       </Box>
       <PortFolioCard />
     </Stack>
