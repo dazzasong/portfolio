094ac935ba0e43be294ccd86a6c56fb58870df82
Darren Song <dazzasong@gmail.com>
2025-05-14 17:50:30 +1000

    added note

--- src/Home/MainContent/WebsiteList.jsx
+++ src/Home/MainContent/WebsiteList.jsx
@@ -26,6 +26,7 @@ export default function WebsiteList() {
     <Stack direction="row" spacing={2}>
       {websites.map((web) => <WebsiteCard img={web.img} to={web.to} disabled={web.disabled} />)}
     </Stack>
+    <Typography fontWeight='bold'>Make sure to check in regularly for new updates!</Typography>
     <Typography>Disabled games are still in development</Typography>
   </Stack>
   );
